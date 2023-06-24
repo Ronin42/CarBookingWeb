99386bf2f4c5f4485c5067d6ccfea163a857f3f8
Ronin42 <after197918@gmail.com>
2023-06-24 20:36:39 +0700

    Add project files.

---
+++ CarBooking.DataAccess/data/ApplicationDBcontext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using CarBooking.Models; 
+
+namespace CarBooking.DataAccess.data
+{
+	public class ApplicationDBcontext : IdentityDbContext
+	{
+		public ApplicationDBcontext(DbContextOptions<ApplicationDBcontext> options) : base(options)
+		{
+
+		}
+		
+
+	}
+}
---
+++ CarBookingWeb/Areas/Employee/Controllers/DistanceMatrixController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using CarBooking.Models;
+
+
+
+namespace BulkyWeb.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class DistanceMatrixController : Controller
+    {
+        private readonly HttpClient httpClient;
+
+        public DistanceMatrixController()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public IActionResult CalculateDistance()
+        {
+
+            return View(new DistanceMatrixViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CalculateDistance(DistanceMatrixViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            string apiKey = "AIzaSyDXIege7mt2EN_DwCZSP6pcPygl7e8sIzo";
+
+            string apiUrl = $"https://maps.googleapis.com/maps/api/distancematrix/json?origins={model.Origin}&destinations={model.Destination}&key={apiKey}";
+
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                dynamic result = JsonConvert.DeserializeObject(jsonResponse);
+
+                model.Distance = result.rows[0].elements[0].distance.text;
+                model.Duration = result.rows[0].elements[0].duration.text;
+
+                //return new JsonResult(model);
+                return View(model);
+            }
+
+            return View();
+
+
+            //return StatusCode((int)response.StatusCode);
+        }
+    }
+}
